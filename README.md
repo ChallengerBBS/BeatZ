0308de41fcb35bc3644394042395cfab738481ed
Sevdalin Vasilev <predator187@abv.bg>
2022-04-30 23:20:23 +0300

    * Add react-bootstrap and display fetched tracks.

---
+++ README.md
@@ -0,0 +1,8 @@
+BeatZ - minified version of Spotify
+
+TODO
+- CRUD Implementation:
+Tracks, Artists and Albums
+- Authentication - Users, Roles, JWT
+- Song bytes transfer over web
+- Host Azure
--- beatz-client/src/Tracks/TracksList.tsx
+++ beatz-client/src/Tracks/TracksList.tsx
@@ -1,15 +1,22 @@
-import {Fragment} from "react";
+import {Fragment, useState} from "react";
+import {Button, Card} from 'react-bootstrap';
 
-const getTracks = () => {
+const getTracks = (setSongs: any) => {
     fetch("https://localhost:7297/api/tracks")
         .then((res) => res.json())
-        .then((data) => console.log(data));
+        .then((data) => {
+            console.log(data)
+            setSongs(data);
+        });
 }
 
-function TrackList(){
+function TrackList() {
+    const [songs, setSongs] = useState([]);
+
     return (
         <Fragment>
-            <button onClick={() => getTracks()}>Load Tracks</button>
+            <Button className="btn btn-primary" type="button" onClick={() => getTracks(setSongs)}>Load Tracks</Button>
+            {songs && songs.map((track) => <Card>{track.trackName}</Card>)}
         </Fragment>
     );
 }
