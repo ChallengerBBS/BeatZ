bd83215004952c8b00adac7365a6254d56a9157a
Sevdalin Vasilev <predator187@abv.bg>
2022-06-12 21:43:05 +0300

    Add custom middlewares

---
+++ BeatZ/Middlewares/NotFoundMiddleware.cs
@@ -0,0 +1,15 @@
+﻿namespace BeatZ.Api.Middlewares
+{
+    public class NotFoundMiddleware: IMiddleware
+    {
+        public Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            if (!context.Response.HasStarted)
+            {
+                context.Response.StatusCode = 404;
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ BeatZ/Middlewares/TrackStreamMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using BeatZ.Infrastructure.Services;
+
+namespace BeatZ.Api.Middlewares
+{
+    public class TrackStreamMiddleware : IMiddleware
+    {
+        private readonly TracksService _tracksService;
+
+        public TrackStreamMiddleware(TracksService tracksService)
+        {
+            this._tracksService = tracksService;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var requestPath = context.Request.Path;
+
+            const string RECORDINGS_PATH = "/recordings/";
+
+            if (!requestPath.HasValue || !requestPath.Value!.StartsWith(RECORDINGS_PATH) || requestPath.Value.Length <= RECORDINGS_PATH.Length)
+            {
+                await next(context);
+                return;
+            }
+
+            string reqTrackId = requestPath.Value.Remove(0, RECORDINGS_PATH.Length);
+
+            int trackId;
+
+            if (!int.TryParse(reqTrackId, out trackId))
+            {
+                context.Response.StatusCode = 400;
+                return;
+            }
+
+            var track = this._tracksService.GetTrack(p=>p.TrackId == trackId);
+        }
+    }
+}
